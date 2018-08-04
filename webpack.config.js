84da4e6a2b8826f5f4e91ff8b3743b042efc6957
Andrew Usher <andrewusher00@gmail.com>
2018-08-04 18:44:43 -0500

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'cheap-module-eval-source-map' : 'nosources-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '/docs')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/docs'),
+    historyApiFallback: true,
+    hot: true,
+    overlay: true,
+    publicPath: '/',
+    port: 8080
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Sudoku',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+}
