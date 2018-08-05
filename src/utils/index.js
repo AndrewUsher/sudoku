a140b87daaf805b9afd16d741879ead4d4004012
Andrew Usher <andrewusher00@gmail.com>
2018-08-04 23:14:33 -0500

    Export all utility functions in index of folder

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+// @flow
+import checkUnique from './checkUnique'
+import getColumn from './getColumn'
+import getNextBox from './getNextBox'
+import getRow from './getRow'
+import puzzleIsComplete from './puzzleIsComplete'
+import puzzleIsSolvable from './puzzleIsSolvable'
+import solvePuzzle from './solvePuzzle'
+
+export {
+  checkUnique,
+  getColumn,
+  getNextBox,
+  getRow,
+  puzzleIsComplete,
+  puzzleIsSolvable,
+  solvePuzzle
+}
